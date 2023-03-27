1f85ef3781a39a90b92e5c7de847c947da333d8b
teristit <dimitri.alexandr5w@yandex.ru>
2023-03-27 18:35:57 +0300

    добавил menu_send.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types
+
 from faq_send import faq_send
 from help_send import help_send
 from settings import TG_TOKEN, ID_ADMIN
@@ -6,9 +8,23 @@ from settings import TG_TOKEN, ID_ADMIN
 bot = telebot.TeleBot(TG_TOKEN)
 
 
+def buttons_return(num):
+    d = tuple()
+    for i in num:
+        d = d + (types.InlineKeyboardButton(i[0], callback_data=i[1]),)
+    return d
+
+
 @bot.message_handler(commands=['start'])
 def get_text_messages(message):
     bot.send_message(message.from_user.id, 'Вы запустили бота')
+    bot.send_message(message.from_user.id, 'ВыБерите действие')
+    buttons = types.InlineKeyboardMarkup(row_width=1)
+
+    b = buttons_return(
+        [('Мурманская область', 'butr1'), ('Архангельская область', 'butr2')])
+    buttons.add(*b)
+    bot.send_message(message.chat.id, reply_markup=buttons)
 
 
 @bot.message_handler(commands=['help'])
@@ -23,14 +39,12 @@ def faq_messages(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text[:4] == 'Дима':
+    if message.text[:4] == 'Админ':
         bot.send_message(ID_ADMIN, message.text)
     elif message.text == 'FAQ':
         faq_send(message, bot)
     elif message.text == 'Вернуться в раздел поддержки':
         help_send(message, bot)
-    else:
-        bot.send_message(message, message.text)
 
 
 bot.polling(none_stop=True)
