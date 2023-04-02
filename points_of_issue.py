e5374e1ae2c58f028869abf06f15fe9f7a88ff66
sp1keer <pav.mikhajlov2018@yandex.ru>
2023-04-02 14:45:10 +0300

    добавил функционал функции points_of_issue

--- points_of_issue.py
+++ points_of_issue.py
@@ -2,7 +2,15 @@ from telebot import types
 
 
 def points_of_issue(message, bot):
-    pass
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
+    points_v = types.KeyboardButton('Пункты выдачи в Вурнарах')
+    points_ch = types.KeyboardButton('Пункты выдачи в Чебоксарах')
+    back = types.KeyboardButton('Вернуться к часто задаваемым вопросам')
+    markup.add(points_v, points_ch, back)
+    bot.send_message(message.from_user.id, 'Наши пункты распологаються в Чувашии')
+    bot.send_message(message.from_user.id, 'А именно: г. Чебоксары, пгт. Вурнары')
+    bot.send_message(message.from_user.id, 'Чтобы посмотреть по каким адресам расположены пункты в интересующем '
+                                           'вас городе или селе нажмите на соответсвующую кнопку', reply_markup=markup)
 
 
 def points_Vurnary(message, bot):
