85414fc76b0c71a643139cab478bc2045f3889b5
sp1keer <pav.mikhajlov2018@yandex.ru>
2023-04-02 11:58:53 +0300

    создал функции ответов на впросы

---
+++ answer_faq.py
@@ -0,0 +1,17 @@
+from telebot import types
+
+
+def first_answer(message, bot):
+    pass
+
+
+def second_answer(message, bot):
+    pass
+
+
+def third_answer(message, bot):
+    pass
+
+
+def fourth_answer(message, bot):
+    pass
