ffd7146ddfbb0572a5c21e04aa4a629074b023fa
sp1keer <pav.mikhajlov2018@yandex.ru>
2023-04-23 16:16:24 +0300

    сделал функцию add_user

---
+++ data/add_user.py
@@ -0,0 +1,17 @@
+import telebot
+from data import db_session
+from data.support_to_user import User
+
+
+def add_user(message, bot, problem):
+    if problem:
+        db_session.global_init("db/support.db")
+        db_sess = db_session.create_session()
+        user = User()
+        user.problem = f"{problem}"
+        user.set_hashed(f"{message.from_user.id}")
+        db_sess.add(user)
+        db_sess.commit()
+        markup = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id,
+                         'Ваше обращение создано и отправлено', reply_markup=markup)
